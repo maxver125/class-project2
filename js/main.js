93f4a54cf0e1ff9a5ba6734606c845eeb6c2b8ef
maxver125 <mverchinski@gmail.com>
2016-06-21 20:17:59 -0700

    updates

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    $(".mvshow").click(function(){
+        $(".contact").fadeIn(3000);
+    });
+    
+    $(".mvHide").click(function(){
+        $(".contact").fadeOut(3000);
+    });
+});
